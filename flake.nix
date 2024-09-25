941462d43855d1b992d4b4e9c9ca6533621d5b82
Bohdan <17168103+liesnikov@users.noreply.github.com>
2024-09-25 16:59:21 +0200

    simplify the flake

--- flake.nix
+++ flake.nix
@@ -14,12 +14,14 @@
       let
         pkgs = import nixpkgs {inherit system;};
         agda2hs-lib = agda2hs.packages.${system}.agda2hs-lib;
+        agda2hsWithPackages = agda2hs.lib.${system}.withPackages;
+        agda2hs-custom = agda2hsWithPackages [agda2hs-lib];
         scope-lib = pkgs.agdaPackages.mkDerivation
           { pname = "scope";
             meta = {};
             version = "0.1.0.0";
             buildInputs = [
-              agda2hs.packages.${system}.agda2hs-lib
+              agda2hs-lib
             ];
             preBuild = ''
               echo "module Everything where" > Everything.agda
@@ -27,15 +29,8 @@
             '';
             src = ./.;
           };
-        helper = agda2hs.lib.${system};
-        agda2hs-drv = pkgs.callPackage (helper.agda2hs-expr) {
-          inherit self;
-          agda2hs = pkgs.haskellPackages.callPackage (helper.agda2hs-pkg "") {};
-          inherit (pkgs.haskellPackages) ghcWithPackages;
-        };
-        agda2hs-custom = agda2hs-drv.withPackages [agda2hs-lib];
         scope-pkg = import ./scope.nix;
-        scope-hs = pkgs.haskell.packages.ghc94.callPackage scope-pkg {agda2hs = agda2hs-custom;};
+        scope-hs = pkgs.haskellPackages.callPackage scope-pkg {agda2hs = agda2hs-custom;};
       in {
         packages = {
           inherit scope-hs scope-lib;
